6340249431846b32686d8257a0a0f1b32c55c028
aakulasaikiran <email@example.com>
2018-07-30 12:29:35 +0530

    commit1

---
+++ StudentMongo_Age_greterthen_9years/src/main/java/com/lsn/repository/comman.java
@@ -0,0 +1,38 @@
+package com.lsn.repository;
+
+import java.util.List;
+
+import com.lsn.domain.Student;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+
+public class comman {
+	public static DBCursor getStudentmorethen(int age){
+		DBCursor cursor =null;
+	try {
+		/*MongoClient mongoClient = new MongoClient("localhost",27017);*/
+		
+		
+		MongoClient mongoClient = new MongoClient();
+		DB db = mongoClient.getDB("db100");
+		DBCollection collection = db.getCollection("student");
+		System.out.println(collection);
+
+		cursor = collection.find(new BasicDBObject("age", new BasicDBObject("$gte",age)));
+		System.out.println();
+		System.out.println(cursor);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	
+}
+	return cursor;
+}
+}
